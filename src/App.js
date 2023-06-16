b7c62239846d7c8a464d8b2e89cddde50fc63465
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-06-15 22:01:27 -0300

    Projeto Biscoito da sorte

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import React, { Component } from "react";
+import './estilo.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textoFrase: "",
+    }
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+    this.frases = ['Siga os bons e aprenda com eles', 'Para vencer na vida, a sabedoria vale muito mais que a força bruta, e o bom senso muito mais que a valentia', 'O riso é a menor distância entre duas pessoas', 'Deixe da laso as preocupações e seja feliz', 'Realize o óbvio, pense no improvável e conquiste o impossível', 'Acredite em milagres, mas não dependa deles', 'A maior barreira para o sucesso é o medo do fracasso'];
+  }
+
+  quebraBiscoito() {
+    let state = this.state;
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+    state.textoFrase = '" ' + this.frases[numeroAleatorio] + ' "';
+    this.setState(state);
+  }
+
   render() {
-    return <div></div>;
+    return <div className="container">
+      <img src={require('./assets/biscoito.png')} className="img" />
+      <Botao nome="Abrir biscoito" acaoBtn={this.quebraBiscoito} />
+      <h3 className="textoFrase">
+        {this.state.textoFrase}
+      </h3>
+    </div>;
+  }
+}
+
+class Botao extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.acaoBtn}>
+          {this.props.nome}
+        </button>
+      </div>
+    )
   }
 }
 
